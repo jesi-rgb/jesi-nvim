fbf7a694a915608b2d554a88549006cbfc082019
Jesús Enrique Rascón <blograso@gmail.com>
2023-04-28 12:12:02 +0200

    add surround

--- lua/plugins/jesi.lua
+++ lua/plugins/jesi.lua
@@ -2,6 +2,17 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
   { "rose-pine/neovim", name = "rose-pine", opts = { disable_italics = true, disable_background = true } },
   { "ThePrimeagen/harpoon" },
   { "nvim-telescope/telescope.nvim" },
