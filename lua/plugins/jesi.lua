22779d100d9c019abccb5ee9acada881e744caf5
Jesús Enrique Rascón <blograso@gmail.com>
2023-05-30 16:39:29 +0200

    add tag highlight

--- lua/plugins/jesi.lua
+++ lua/plugins/jesi.lua
@@ -1,23 +1,24 @@
 return {
+  -- disabled plugins
   { "rcarriga/nvim-notify", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+
+  -- enabled or configured plugins
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
+      require("nvim-surround").setup({})
     end,
   },
-  { "echasnovski/mini.indentscope", enabled = false },
-
   { "rose-pine/neovim", name = "rose-pine", opts = { disable_italics = true, disable_background = true } },
   { "ThePrimeagen/harpoon" },
   { "nvim-telescope/telescope.nvim" },
   { "sbdchd/neoformat" },
+  { "leafOfTree/vim-matchtag" },
   {
     "LazyVim/LazyVim",
     opts = {
