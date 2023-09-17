9b40e388611adaed3d9cd3d808248131d9b61b09
Jesús Enrique Rascón <blograso@gmail.com>
2023-09-17 12:25:19 +0200

    change polling rate

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -140,7 +140,7 @@ require("nvim-web-devicons").setup({})
 
 local battery = require("battery")
 battery.setup({
-  update_rate_seconds = 30, -- Number of seconds between checking battery status
+  update_rate_seconds = 60, -- Number of seconds between checking battery status
   show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
   show_plugged_icon = true, -- If true show a cable icon alongside the battery icon when plugged in
   show_unplugged_icon = false, -- When true show a diconnected cable icon when not plugged in
