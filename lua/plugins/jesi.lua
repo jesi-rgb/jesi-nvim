c60f3ae94fe824a5bdd32468f54b4ed48cce4687
Jesús Enrique Rascón <blograso@gmail.com>
2024-02-24 12:09:29 +0100

    backup config

--- lua/plugins/jesi.lua
+++ lua/plugins/jesi.lua
@@ -39,6 +39,7 @@ return {
   { "othree/html5.vim" },
   { "pangloss/vim-javascript" },
   { "evanleck/vim-svelte" },
+  { "Exafunction/codeium.vim", event = "BufEnter" },
 
   {
     "LazyVim/LazyVim",
