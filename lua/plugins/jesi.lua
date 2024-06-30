2096691c980ab3550a85f07bfc66350c036cd826
Jesús Enrique Rascón <blograso@gmail.com>
2024-06-30 13:49:30 +0200

    remove lazygit

--- lua/plugins/jesi.lua
+++ lua/plugins/jesi.lua
@@ -13,6 +13,10 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     enabled = false,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    enabled = false,
+  },
 
   -- { "stevearc/dressing.nvim", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
@@ -47,25 +51,7 @@ return {
   { "leafOfTree/vim-matchtag" },
 
   { "prettier/vim-prettier" },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
+
   { "chrisbra/Colorizer" },
 
   { "sheerun/vim-polyglot" },
